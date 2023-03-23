1de9f55703a882915e477697c8d50ca4fb7fda8d
LucasAraujo1306 <lucasisaac007@gmail.com>
2023-03-23 05:45:08 -0300

    variações possivel no banco de dados noSQL

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -1,12 +1,56 @@
+import { log } from 'console';
 import { Request, Response } from 'express';
 
 import { Product } from '../models/Product';
+import User from '../models/User'
 
-export const home = (req: Request, res: Response)=>{
+export const home = async (req: Request, res: Response) => {
+
+    //Para consulta mais de um registro no banco de dados retorna array de objetos
+    let usuariosFind = await User.find({});
+
+    //Para consulta um registro no banco de dados retorna o objeto
+    let usuariosOne = await User.findOne({
+        email: 'beltrano@gmail.com'
+    });
+
+    //Para consulta um registro pelo Id no banco de dados retorna um unico objeto
+    let usuariosById = await User.findById('641bdda9ad958e4bb1abf443');
+    //caso não entre com o id correto ele retorna null
+
+    let usuariosFindEmail = await User.find({
+        email: 'fulano@gmail.com',
+        age: 21
+    })
+    //caso não encontre usuario retorna um array vazio
+
+
+    //caso encontre usuario retorna um array com o objeto
+    let pegaNomeDentroObj = await User.find({
+        "name.firstName": 'fulano'
+    })
+    //caso não encontre usuario retorna um array vazio
+
+    //caso encontre usuario retorna varios usuarios dentor de array com os objetos
+    let procurarUsuarioComArray = await User.find({
+        interests: 'node'
+    })
+    //caso não encontre usuario retorna um array vazio
+
+
+    //gt - greater then = maior
+    //gte - greater then or equal = maior ou igual
+    //lt - less then = menor
+    //lte - less then or equal = menor ou igual
+    let usuariosFindAge = await User.find({
+        age: { $gt: 21 }
+    })
+
+
     let age: number = 90;
     let showOld: boolean = false;
 
-    if(age > 50) {
+    if (age > 50) {
         showOld = true;
     }
 
